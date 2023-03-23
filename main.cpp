018d16720044758be8e5788c9a12752254d4fa4a
Instituto de Ciencias Exatas <ice@lab05m10.nrc.ice.ufjf.br>
2023-03-23 15:34:57 -0300

    update main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
+void imprimeVet(int vet[], int n){
+    for(int i=0; i<n; i++){
+        cout<< vet[i] << " ";
+    }cout << endl;
+}
+void troca(int * a, int * b){
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+
+}
+
+void BubbleSort(int vet[], int n){
+    for(int i= n-1; i>=0; i--){
+        for(int j=0; j< i; j++){
+            if(vet[j]> vet[j+1]){
+                troca(&vet[j], &vet[j+1]);
+            }
+        }
+    }
+
+}
+
+void SelectSort(int vet[], int n){
+    for(int i=0; i<n; i++){
+        
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n= 5;
+    int vet[n] = {4, 2, 3, 6, 1};
+    imprimeVet(vet, n);
+    BubbleSort(vet, n);
+    imprimeVet(vet, n);
     return 0;
 }
